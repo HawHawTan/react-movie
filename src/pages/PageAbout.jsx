c8deb05e4e669beca3c027633dc1ff916fbffeb5
Gustavo <gustavoyamamoto@gmail.com>
2024-07-29 14:28:16 -0700

    Added useeffect on about page (Route) and title

--- src/pages/PageAbout.jsx
+++ src/pages/PageAbout.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import { useEffect } from "react"
 
-function PageAbout() {
+const PageAbout = () => {
+
+    useEffect(() => {
+        document.title = 'About | ${appTitle}';
+    }, []);
+
     return (
         <div>
             <Header />
-            
+    
             <section className='about'>
                 <h2>Welcome to CURTAIN DROP</h2>
                 <h3>The stage for all things cinema!</h3>
                 <p>Dive into a whirlwind of movie magic with our vibrant movie database website. From timeless classics to the latest blockbusters, we've got it all under one digital roof. Let the movie marathon begin at Curtain Drop - where the curtain will rise on your cinematic journey!</p>
             </section>
-
+    
             <Footer />
         </div>
-    )
-}
+    );
+};
+
+
 
 export default PageAbout
