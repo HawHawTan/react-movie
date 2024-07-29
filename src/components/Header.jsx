e7efdcedc88a4f1a9ec7dfb701701c7b380b7ecb
Kaleb Link <kaleb.s.link@gmail.com>
2024-07-29 12:18:07 -0700

    added navlinks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,7 @@ function Header() {
     return (
         <header>
             <img className='logo-svg' src="./src/media/logo-curtaindrop.svg" alt="Curtain Drop Logo" />
+            <img className='hamburger-menu-svg' src="./src/media/hamburger-menu.svg" alt="Curtain Drop Logo" />
         </header>
     )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 function Nav() {
     return (
-        <div>Nav</div>
+        <nav>
+            <ul>
+                <li><NavLink to="/">Home</NavLink></li>
+                <li><NavLink to="/favourites">Favourites</NavLink></li>
+                <li><NavLink to="/about">About</NavLink></li>
+            </ul>
+        </nav>
     )
 }
 
