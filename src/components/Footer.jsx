17632f1211094d3c823369a5317ba5d7c43f4b0f
Gustavo <gustavoyamamoto@gmail.com>
2024-07-29 11:10:47 -0700

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,16 @@ import { getYear } from '../utilities/getDates';
 
 const Footer = ({ copyright, author }) => (
 	<footer>
+        <figure>
+            <img src="../src/media/logo-curtaindrop.svg" alt="" />
+        </figure>
         <p>&copy; {copyright} {author}</p>
+        <p>For education purpose only.</p>
     </footer>
 );
 
 Footer.defaultProps = {
+    author: 'Kaleb/Haw Tan/Gustavo',
     copyright: getYear()
 }
 
