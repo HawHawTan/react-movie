a5ea946cdcd431ac29c8deb084005d18cf7f6796
Kaleb Link <kaleb.s.link@gmail.com>
2024-08-20 15:53:07 -0700

    moving to favourites branch

--- src/pages/PageFavourites.jsx
+++ src/pages/PageFavourites.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+// Find way to set global state
+// pull in global data of favourite movies
+
 function PageFavourites() {
     return (
         <div>PageFavourites</div>
