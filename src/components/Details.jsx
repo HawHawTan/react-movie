27fdb67397df7104827ffe78bb1dec71e7e30363
Gustavo <gustavoyamamoto@gmail.com>
2024-08-21 15:52:26 -0700

    Added detail page-mobile

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -18,21 +18,50 @@ const Details = () => {
 
     if (!movie) return <div>Loading...</div>;
 
+    function formatDateWithOrdinal(dateString) {
+        const date = new Date(dateString);
+        
+        const day = date.getDate();
+        const month = date.toLocaleString('en-US', { month: 'long' });
+        const year = date.getFullYear();
+    
+        function getOrdinalSuffix(n) {
+            const s = ["th", "st", "nd", "rd"];
+            const v = n % 100;
+            return s[(v - 20) % 10] || s[v] || s[0];
+        }
+    
+        return `${month} ${day}${getOrdinalSuffix(day)}, ${year}`;
+      }
+
+      function formatRating(rating) {
+        return `${(rating * 10).toFixed(0)}`;
+      }
+
+
     return (
-        <div className="movie-details">
+        <section className="movie-details">
+                <img className='backdrop-movie' src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} alt={movie.title} />
+
+
             <div className="movie-header">
                 {/* Display the movie's poster */}
-                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+                <img className='poster-movie' src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
                 
                 {/* Movie information */}
                 <div className="movie-info">
-                    <h1>{movie.title}</h1>
-                    <p>Release Date: {new Date(movie.release_date).toDateString()}</p>
-                    <p>Rating: {movie.vote_average} / 10</p>
-                    <p>{movie.overview}</p>
+                    <h1 className='title-detail'>{movie.title}</h1>
+                    <p className='date-detail'>{formatDateWithOrdinal(movie.release_date)}</p>
+
+
+                    <p className='rating-detail'>
+                        <span className='rating-value'>{formatRating(movie.vote_average)}</span>
+                    </p>
+
+                    <p className='p-detail'>{movie.overview}</p>
                 </div>
             </div>
-        </div>
+        </section>
     );
 };
 
