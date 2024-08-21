439d0c9bdd8ab761e202551491b807456126118a
Kaleb Link <kaleb.s.link@gmail.com>
2024-08-20 23:27:47 -0700

    what the hell am I doing

--- src/pages/PageFavourites.jsx
+++ src/pages/PageFavourites.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 // Find way to set global state
 // pull in global data of favourite movies
 
 function PageFavourites() {
+
+    const movieFavs = useSelector((state) => state.favs.items);
+
     return (
-        <div>PageFavourites</div>
+        <section>
+            <h2>Your Favourites</h2>
+
+        </section>
     )
 }
 
