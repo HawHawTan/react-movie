d2c9beb7fad491e0082b02d61396d62a8412b606
Haw Haw Tan <113495100+HawHawTan@users.noreply.github.com>
2024-08-21 17:13:17 -0700

    added svg for movie category, created mobile category

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,39 +2,38 @@ import { useEffect, useState } from "react";
 import { Link } from 'react-router-dom'
 
 
-function Cards() {
-    const api = import.meta.env.VITE_API_KEY;
-    const [movie,setMovie] = useState(null);
-    const [category, setCategory] = useState('now_playing');
-
-    const getMovie =(c) =>{
-      fetch(`https://api.themoviedb.org/3/movie/${c}?language=en-US&page=1&api_key=${api}` )
-      .then(response => response.json())
-      .then(response => setMovie(response.results))
-      .catch(err => console.error(err));
-    }
-    useEffect(()=>{
+function Cards({getMovie,movies}){
+  // const [movie, setMovie] = useState(null);
+  const api = import.meta.env.VITE_API_KEY;
+  // const getMovie = (c) => {
+  //   fetch(`https://api.themoviedb.org/3/movie/${c}?language=en-US&page=1&api_key=${api}`)
+  //       .then(response => response.json())
+  //       .then(response => setMovie(response.results))
+  //       .catch(err => console.error(err));
+  // };
+  // console.log(movies);
+  
+    useEffect(() => {
       getMovie('now_playing');
-    },[])
-
-
+  }, []);
   return (
+    
     <div>
-        <div id="movies-category">
-          <button onClick={() => getMovie('now_playing')}>Now Playing</button>
+       <div id="movies-category-desktop">
           <button onClick={() => getMovie('popular')}>Popular</button>
           <button onClick={() => getMovie('top_rated')}>Top Rated</button>
           <button onClick={() => getMovie('upcoming')}>Upcoming</button>
+          <button onClick={() => getMovie('now_playing')}>Now Playing</button>
         </div >
         <ul>
-            { movie && movie.map(item => {
+            { movies && movies.map(item => {
                 return(
                     <li key={item.id} >
-                      <Link to={`/details/${item.id}`}><img src={`https://image.tmdb.org/t/p/w342${item.poster_path}`} alt={item.title} /></Link>
+                      <Link to={`/details/${item.id}`}><img src={`https://image.tmdb.org/t/p/w342${item.poster_path}`} alt={item.title}/></Link>
                       <div className="overlay">
                         <h3>{item.title}</h3>
                         <p>{item.overview}</p>
-                        <button>More Info</button>
+                        <Link to={`/details/${item.id}`}><button>More Info</button></Link>
                       </div>
                     </li>
                 )
--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import Cards from '../components/Cards';
+import Mobile_category from '../components/Mobile_category';
+import { useEffect, useState } from "react";
 
 function PageHome() {
+    const api = import.meta.env.VITE_API_KEY;
+    const [movie, setMovie] = useState(null);
+
+    const getMovie = (c) => {
+        fetch(`https://api.themoviedb.org/3/movie/${c}?language=en-US&page=1&api_key=${api}`)
+            .then(response => response.json())
+            .then(response => setMovie(response.results))
+            .catch(err => console.error(err));
+    };
+    
     return (
         <main>
-            <Cards/>
+              <Cards getMovie={getMovie} movies={movie} />
+              <Mobile_category getMovie={getMovie} />
         </main>
     )
 }
