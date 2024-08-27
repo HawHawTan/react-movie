8526c3fb1f26353206aa7cb2ce334fe12680b11d
Gustavo <gustavoyamamoto@gmail.com>
2024-08-26 18:26:54 -0700

    Deploying code added

--- src/globals/globalVariables.js
+++ src/globals/globalVariables.js
@@ -1,1 +1,5 @@
 export const appTitle = 'Curtain Drop';
+
+const APP_FOLDER_NAME = "curtain-drop";
+const OTHER_GLOBAL = "other-global";
+export { APP_FOLDER_NAME, OTHER_GLOBAL}
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+  base: '/curtain-drop/',
+  build: {
+    outDir: 'curtain-drop'
+  },
+  plugins: [reactRefresh()],
+  plugins: [react()]
 })
