64ff858e75298c484ef448f66d99df7845b29bf4
Gustavo <gustavoyamamoto@gmail.com>
2024-08-26 18:48:58 -0700

    Logos

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -15,7 +15,7 @@ function PageNotFound(){
 			<section className='not-found-wrapper'>
 				<p className='not-found-p'>Ooops! The page you are looking for doesn't exist.</p>
 				<p className='not-found-p2'><Link to="/">Click here to go back.</Link></p>
-			<img className='logo-svg-not-found' src="/src/media/logo-curtaindrop.svg" alt="Curtain Drop Logo" />
+			<img className='logo-svg-not-found' src="/curtain-drop/src/media/logo-curtaindrop.svg" alt="Curtain Drop Logo" />
 			</section>
 		</main>
 	);
